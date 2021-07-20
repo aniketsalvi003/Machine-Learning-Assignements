4168ca37d76887ea3b92c49e1678a7ba7222d4b3
aniketsalvi003 <80400849+aniketsalvi003@users.noreply.github.com>
2021-07-20 10:25:34 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,48 @@
+# importing the necessary dependencies
+import pickle
+
+from flask import Flask, render_template, request
+from flask_cors import cross_origin
+import flasgger
+
+app = Flask(__name__)  # initializing a flask app
+
+
+@app.route('/', methods=['GET'])  # route to display the home page
+@cross_origin()
+def homePage():
+    return render_template("index.html")
+
+
+@app.route('/predict', methods=['POST', 'GET'])  # route to show the predictions in a web UI
+@cross_origin()
+def index():
+    if request.method == 'POST':
+        try:
+            #  reading the inputs given by the user
+            ZN = float(request.form['ZN'])
+            CHAS = float(request.form['CHAS'])
+            RM = float(request.form['RM'])
+            DIS = float(request.form['DIS'])
+            B = float(request.form['B'])
+            LSTAT = float(request.form['LSTAT'])
+            PTRATIO = float(request.form['PTRATIO'])
+
+            filename = 'model.pkl'
+            loaded_model = pickle.load(open(filename, 'rb'))  # loading the model file from the storage
+            # predictions using the loaded model file
+            prediction = loaded_model.predict(['ZN','CHAS','RM','DIS','B','LSTAT','PTRATIO'])
+            print('prediction is', prediction)
+            # showing the prediction results in a UI
+            return render_template('results.html', prediction=prediction)
+        except Exception as e:
+            print('The Exception message is: ', e)
+            return 'something is wrong'
+    # return render_template('results.html')
+    else:
+        return render_template('index.html')
+
+
+if __name__ == "__main__":
+    # app.run(host='127.0.0.1', port=8001, debug=True)
+    app.run(debug=True)  # running the app
